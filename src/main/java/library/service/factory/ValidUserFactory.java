edc41de0347c2110493c8bb5e0885e9419c95c30
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-04-06 17:56:18 +0300

    Added service.

---
+++ src/main/java/library/service/factory/ValidUserFactory.java
@@ -0,0 +1,21 @@
+package library.service.factory;
+
+import library.exception.InvalidEmailException;
+import library.model.enums.UserType;
+import library.model.user.ReaderUser;
+import library.model.user.User;
+import library.model.user.WriterUser;
+import library.util.validators.EmailValidator;
+
+public class ValidUserFactory {
+    public <E extends User> E getValidUser(UserType userType, String name,
+                                           String surname, String email, String password) throws InvalidEmailException {
+        EmailValidator.validEmail(email);
+        switch (userType){
+            case REGULAR_USER:
+                return (E) new ReaderUser(name, surname, email, password);
+            default:
+                throw new IllegalStateException("Unexpected value: " + userType);
+        }
+    }
+}
