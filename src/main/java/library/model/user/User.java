18d2f743160cdf305e5101b05786a490d8bdf15f
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-05-12 18:08:50 +0300

    Removed annotations for json.

--- src/main/java/library/model/user/User.java
+++ src/main/java/library/model/user/User.java
@@ -1,34 +1,15 @@
 package library.model.user;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import library.exception.BookNotFoundException;
 import library.model.book.Book;
-import library.util.collapser.BookListCollapser;
 import lombok.Getter;
-import lombok.experimental.FieldNameConstants;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
-        property = "type",
-        visible = true)
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = WriterUser.class, name = "WriterUser"),
-        @JsonSubTypes.Type(value = ReaderUser.class, name = "ReaderUser")}
-)
-
 @Getter
 public abstract class User implements Borrow {
 
-    @JsonProperty("type")
-    @FieldNameConstants.Exclude
-    private String type = this.getClass().getSimpleName(); // Needed for json polymorphic deserialization
     private String name;
     private String surname;
     private String email;
