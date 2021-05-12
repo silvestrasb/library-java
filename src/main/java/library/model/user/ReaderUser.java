593bccfbf3abd8f6a21aea77563af66887fd070c
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-05-12 21:39:41 +0300

    Added lombok annotations to ReaderUser.

--- src/main/java/library/model/user/ReaderUser.java
+++ src/main/java/library/model/user/ReaderUser.java
@@ -1,9 +1,15 @@
 package library.model.user;
 
 import library.model.book.Book;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class ReaderUser extends User {
     List<Book> borrowedBooks;
 }
