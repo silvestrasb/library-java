f2d6cd4bb3c475b43db3ecabf88c19a7695bc9f7
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-05-12 21:25:02 +0300

    Deleted everything in ReaderUser class..

--- src/main/java/library/model/user/ReaderUser.java
+++ src/main/java/library/model/user/ReaderUser.java
@@ -2,11 +2,5 @@ package library.model.user;
 
 public class ReaderUser extends User {
 
-    public ReaderUser() {
-    }
-
-    public ReaderUser(String name, String surname, String email, String password) {
-        super(name, surname, email, password);
-    }
-
 }
+
