9b5bf2b52306381ae9c548fcae1756a2c3364e7d
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-05-12 17:07:49 +0300

    Removed quantity feature and interfaces for Book class.

--- src/main/java/library/model/book/Book.java
+++ src/main/java/library/model/book/Book.java
@@ -1,23 +1,11 @@
 package library.model.book;
 
-public class Book implements Quantifiable, Releasable {
+public class Book {
 
     private String title;
     private String authorsName;
     private String authorsSurname;
     private String genre;
-    private int quantity;
-
-    public Book() {
-    }
-
-    public Book(String title, String authorsName, String authorsSurname, String genre, int quantity) {
-        this.title = title;
-        this.authorsName = authorsName;
-        this.authorsSurname = authorsSurname;
-        this.genre = genre;
-        this.quantity = quantity;
-    }
 
     @Override
     public String getTitle() {
@@ -35,48 +23,6 @@ public class Book implements Quantifiable, Releasable {
     @Override
     public String getGenre() {
         return this.genre;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                ", authorsName='" + authorsName + '\'' +
-                ", authorsSurname='" + authorsSurname + '\'' +
-                ", genre='" + genre + '\'' +
-                ", quantity=" + quantity +
-                '}';
-    }
-
-    @Override
-    public int getQuantity() {
-        return this.quantity;
-    }
-
-    @Override
-    public void removeOne() {
-        this.quantity--;
-    }
-
-    @Override
-    public void addOne() {
-        this.quantity++;
-    }
-
-    @Override
-    public void add(int quantity) {
-        this.quantity += quantity;
-    }
-
-    @Override
-    public void remove(int quantity) {
-        this.quantity -= quantity;
-    }
-
-    @Override
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
     }
 
 }
