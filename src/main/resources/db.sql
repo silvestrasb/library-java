39601c2f8fd9e7bd7c1412f17d27ea869a3cc4b7
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-05-17 13:22:44 +0300

    Done.

---
+++ src/main/resources/db.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS reader_user;
+DROP TABLE IF EXISTS book;
+
+-- reader user
+CREATE TABLE reader_user
+(
+    id serial NOT NULL,
+    name character varying(45),
+    surname character varying(45),
+    email character varying(60),
+    password character varying(45),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE reader_user
+    OWNER to adminuser;
+
+-- movie
+CREATE TABLE book
+(
+    id integer NOT NULL DEFAULT nextval('book_id_seq'::regclass),
+    title character varying(45) COLLATE pg_catalog."default",
+    author_name character varying(45) COLLATE pg_catalog."default",
+    author_surname character varying(45) COLLATE pg_catalog."default",
+    genre character varying(45) COLLATE pg_catalog."default",
+    user_id integer,
+    CONSTRAINT book_pkey PRIMARY KEY (id),
+    CONSTRAINT "FK_USER_ID" FOREIGN KEY (user_id)
+        REFERENCES public.reader_user (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+ALTER TABLE book
+    OWNER TO adminuser;
+
+-- test data
+INSERT INTO reader_user (first_name, last_name, email, password)
+VALUES ('Saulius',
+        'Nauseda',
+        'nauseda@lithuania.lt',
+        'LTUETAXOROXO');
+
+INSERT INTO book (title, author_name, author_surname, genre, user_id)
+VALUES ("Horror of sleeping", 'Thomas', 'Sigolas', "horror", null);
+
