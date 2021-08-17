922b8685cbb4dabf11a2312d3df8c6f6bfc0696a
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-08-17 19:53:05 +0300

    Refactoring.

--- src/main/java/library/entity/Book.java
+++ src/main/java/library/entity/Book.java
@@ -3,11 +3,14 @@ package library.entity;
 import library.dto.request.CreateBookRequest;
 import library.dto.request.UpdateBookRequest;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 
 @Entity
