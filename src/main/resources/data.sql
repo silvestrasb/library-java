20d2459933686ed8de58b35cea28aa633b5ef959
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-08-23 21:50:27 +0300

    Profiles: dev, test, prod.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,13 @@
+DROP TABLE IF EXISTS "user" CASCADE;
+CREATE TABLE "user"
+(
+    id       BIGSERIAL PRIMARY KEY  NOT NULL,
+    name     CHARACTER VARYING(255) NOT NULL,
+    surname  CHARACTER VARYING(255) NOT NULL,
+    email    CHARACTER VARYING(255) NOT NULL,
+    password CHARACTER VARYING(255) NOT NULL
+);
+
 DROP TABLE IF EXISTS book CASCADE;
 CREATE TABLE book
 (
@@ -14,20 +24,10 @@ VALUES ('Anykščių Šilelis', 'Antanas Baranauskas', 'poem', null),
        ('Altorių šešėly', 'Vincas Mykolaitis-Putinas', 'psichological novel', null);
 
 
-DROP TABLE IF EXISTS "user" CASCADE;
-CREATE TABLE "user"
-(
-    id       BIGSERIAL PRIMARY KEY  NOT NULL,
-    name     CHARACTER VARYING(255) NOT NULL,
-    surname  CHARACTER VARYING(255) NOT NULL,
-    email    CHARACTER VARYING(255) NOT NULL,
-    password CHARACTER VARYING(255) NOT NULL
-);
-
 INSERT INTO "user" (name, surname, email, password)
 VALUES ('ohn', 'Doe', 'john.doe@mail.com', 'johny123'),
      ('Mary', 'Frank', 'marry.frank@mail.com', 'mary123'),
-     ('Vanessa', 'Bank', 'vanessa.bank@mail.com', 'vanessa123')
+     ('Vanessa', 'Bank', 'vanessa.bank@mail.com', 'vanessa123');
 
 
 DROP TABLE IF EXISTS role CASCADE;
