2b16ddcfe1b7e160f094164dd1b5b6efd910756e
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-08-25 01:27:43 +0300

    Roles to UserDTO.

--- src/main/java/library/dto/UserDTO.java
+++ src/main/java/library/dto/UserDTO.java
@@ -1,8 +1,11 @@
 package library.dto;
 
+import library.entity.Role;
 import library.entity.User;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -16,10 +19,13 @@ public class UserDTO {
 
     private String email;
 
+    private Set<Role> roles;
+
     public UserDTO(User user) {
         this.id = user.getId();
         this.name = user.getName();
         this.surname = user.getSurname();
         this.email = user.getEmail();
+        this.roles = user.getRoles();
     }
 }
